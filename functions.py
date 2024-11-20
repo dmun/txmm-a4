1a846e2ff05d072bb760673bbf170883406ec823
dmun <davidru01@live.nl>
2024-11-20 21:52:58 +0100

    chagnes

--- functions.py
+++ functions.py
@@ -9,46 +9,31 @@ from nltk.chunk import RegexpParser
 from nltk import Tree
 
 
+TOP_K = 1000
+
+
+def get_top_k_words(data, k):
+    all_text = " ".join(data['text'].dropna().astype(str))
+    words = re.findall(r'\b\w+\b', all_text.lower())
+    word_counts = Counter(words)
+    top_k_words = word_counts.most_common(k)
+    return [word for word, _ in top_k_words]
+
+
+# TOP_K_WORDS = get_top_k_words(train_data, TOP_K)
+
 POS_TAGS = [
-    "CC",
-    "CD",
-    "DT",
-    "EX",
-    "FW",
-    "IN",
-    "JJ",
-    "JJR",
-    "JJS",
-    "LS",
-    "MD",
-    "NN",
-    "NNS",
-    "NNP",
-    "NNPS",
-    "PDT",
-    "POS",
-    "PRP",
-    "PRP$",
-    "RB",
-    "RBR",
-    "RBS",
-    "RP",
-    "SYM",
-    "TO",
-    "UH",
-    "VB",
-    "VBD",
-    "VBG",
-    "VBN",
-    "VBP",
-    "VBZ",
-    "WDT",
-    "WP",
-    "WP$",
-    "WRB",
+    'CC', 'CD', 'DT', 'EX', 'FW', 'IN', 'JJ', 'JJR', 'JJS',
+    'LS', 'MD', 'NN', 'NNS', 'NNP', 'NNPS', 'PDT', 'POS',
+    'PRP', 'PRP$', 'RB', 'RBR', 'RBS', 'RP', 'SYM', 'TO',
+    'UH', 'VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ', 'WDT',
+    'WP', 'WP$', 'WRB'
 ]
 
-PHRASE = {"NP": r"{<DT>?<JJ>*<NN.*>+}", "VP": r"{<VB.*><RB.*>?<VB.*>?}"}
+PHRASE = {
+    'NP': r'{<DT>?<JJ>*<NN.*>+}',
+    'VP': r'{<VB.*><RB.*>?<VB.*>?}'
+}
 
 
 def get_tokenized_data(data: pd.DataFrame):
@@ -92,6 +77,7 @@ def get_parentheses_frequency(text: str):
 
 
 def get_num_emoticons(text: str):
+    # return len(re.findall(r"[:;]-?\)", text))
     return len(re.findall(r"[:;]-?[\)\)\[\]]", text))
 
 
@@ -103,19 +89,19 @@ def get_avg_word_length(tokens: list[str]):
 def get_num_function_word(text: str):
     words = text.split()
     function_word_count = {
-        function_word: words.count(function_word)
+        f'fn:{function_word}': words.count(function_word)
         for function_word in ENGLISH_STOP_WORDS
     }
     return pd.Series(function_word_count)
 
 
 def get_num_letter(text: str):
-    letter_count = {char: text.lower().count(char) for char in string.ascii_lowercase}
+    letter_count = {f'let:{char}': text.lower().count(char) for char in string.ascii_letters}
     return pd.Series(letter_count)
 
 
 def get_num_no_vowel_words(tokens: list[str]):
-    return sum([len(re.findall(r"[aeiou]", text)) for text in tokens])
+    return sum(1 for text in tokens if not re.search(r"[aeiou]", text) and re.search(r"\w", text))
 
 
 def get_num_whitespaces(text: str):
@@ -130,13 +116,20 @@ def get_num_double_quotes(text: str):
     return len(re.findall(r'"', text))
 
 
+def get_num_uppercase_letters(text: str):
+    return sum(1 for char in text if char.isupper())
+
+
+def get_num_digit(text: str):
+    return sum(1 for char in text if char.isdigit())
+
 # def get_num_tokens(tokens: list[str], word: str):
 #     return sum([1 for token in tokens if token == word])
 
 
 def get_num_pos_tags(tokens: list[str]):
     counts = Counter(tag for _, tag in nltk.pos_tag(tokens))
-    return pd.Series({tag: counts.get(tag, 0) for tag in POS_TAGS})
+    return pd.Series({f'pos:{tag}': counts.get(tag, 0) for tag in POS_TAGS})
 
 
 def get_num_phrase(tokens: list[str]):
@@ -149,27 +142,34 @@ def get_num_phrase(tokens: list[str]):
             phrase_counts[subtree.label()] += 1
     return pd.Series(phrase_counts)
 
+# def get_count_top_k_words(tokens: list[str]):
+#     token_counts = Counter(tokens)
+#     return pd.Series({word: token_counts.get(word, 0) for word in TOP_K_WORDS})
+
 
 def extract_features(data: pd.DataFrame):
     df = get_tokenized_data(data)
-    df["avg_num_chars_per_token"] = df["tokens"].parallel_apply(get_avg_num_token_chars)
-    df["num_chars"] = df["text"].parallel_apply(len)
-    df["num_sents"] = df["text"].parallel_apply(get_num_sentences)
-    df["num_alpha"] = df["text"].parallel_apply(get_num_alphabet)
-    df["num_punct"] = df["text"].parallel_apply(get_num_punctuation)
-    df["num_punct2"] = df["text"].parallel_apply(get_num_punctuation, n=2)
-    df["num_punct3"] = df["text"].parallel_apply(get_num_punctuation, n=3)
-    df["num_contractions"] = df["text"].parallel_apply(get_num_contractions)
-    df["num_parentheses"] = df["text"].parallel_apply(get_num_parentheses)
-    # df["parentheses_frequency"] = df["text"].parallel_apply(get_parentheses_frequency)
-    df["num_emoticons"] = df["text"].parallel_apply(get_num_emoticons)
-    df["num_no_vowel_words"] = df["tokens"].parallel_apply(get_num_no_vowel_words)
-    df["avg_word_length"] = df["tokens"].parallel_apply(get_avg_word_length)
-    df["num_single_quotes"] = df["text"].parallel_apply(get_num_single_quotes)
-    df["num_double_quotes"] = df["text"].parallel_apply(get_num_double_quotes)
-    df["num_whitespaces"] = df["text"].parallel_apply(get_num_whitespaces)
-    df = pd.concat([df, df["text"].parallel_apply(get_num_function_word)], axis=1)
-    df = pd.concat([df, df["text"].parallel_apply(get_num_letter)], axis=1)
-    df = pd.concat([df, df["tokens"].parallel_apply(get_num_pos_tags)], axis=1)
-    # df = pd.concat([df, df["tokens"].parallel_apply(get_num_phrase)], axis=1)
+    df["avg_num_chars_per_token"] = df["tokens"].parallel_apply(get_avg_num_token_chars) # 0.06
+    df["num_chars"] = df["text"].parallel_apply(len) #0.059
+    df["num_sents"] = df["text"].parallel_apply(get_num_sentences) # 0.12
+    df["num_alpha"] = df["text"].parallel_apply(get_num_alphabet) #0.067
+    df["num_punct"] = df["text"].parallel_apply(get_num_punctuation) #0.103
+    df["num_punct2"] = df["text"].parallel_apply(get_num_punctuation, n=2) #0.082
+    df["num_punct3"] = df["text"].parallel_apply(get_num_punctuation, n=3) #0.060
+    df["num_contractions"] = df["text"].parallel_apply(get_num_contractions) #0.039
+    df["num_parentheses"] = df["text"].parallel_apply(get_num_parentheses) #0.053
+    df["parentheses_frequency"] = df["text"].parallel_apply(get_parentheses_frequency) # 0.062
+    df["num_emoticons"] = df["text"].parallel_apply(get_num_emoticons) #0.007
+    df["num_uppercase_letters"] = df["text"].parallel_apply(get_num_uppercase_letters) # 0.11
+    df["num_digit"] = df["text"].parallel_apply(get_num_digit) # 0.11
+    df["num_no_vowel_words"] = df["tokens"].parallel_apply(get_num_no_vowel_words) #0.126
+    df["avg_word_length"] = df["tokens"].parallel_apply(get_avg_word_length) #0.070
+    df["num_single_quotes"] = df["text"].parallel_apply(get_num_single_quotes) #0.025
+    df["num_double_quotes"] = df["text"].parallel_apply(get_num_double_quotes) #0.100
+    df["num_whitespaces"] = df["text"].parallel_apply(get_num_whitespaces) #0.064
+    df = pd.concat([df, df["text"].parallel_apply(get_num_function_word)], axis=1) #0.764
+    df = pd.concat([df, df["text"].parallel_apply(get_num_letter)], axis=1) #0.627
+    df = pd.concat([df, df["tokens"].parallel_apply(get_num_pos_tags)], axis=1) #0.656
+    df = pd.concat([df, df["tokens"].parallel_apply(get_num_phrase)], axis=1) #0.090
+    # df = pd.concat([df, df["tokens"].parallel_apply(get_count_top_k_words)], axis=1)
     return df.drop(["Unnamed: 0", "text", "tokens", "author"], axis=1)
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score, f1_score
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -9,6 +10,7 @@ import functions as F
 import string
 from pandarallel import pandarallel
 from joblib import Parallel, delayed
+from itertools import chain
 
 # %%
 np.random.seed(42)
@@ -34,81 +36,24 @@ X_test = F.extract_features(test_data).fillna(0)
 # X_dev = F.extract_features(dev_data).fillna(0)
 
 # %%
-DEFAULT_MAX_DEPTH = 30
-DEFAULT_N_ESTIMATORS = 100
-DEFAULT_RANDOM_STATE = 42
+def perform_ablation(features):
+    columns = list(chain.from_iterable(features))
+    X_train_filtered = X_train.filter(items=columns)
+    X_test_filtered = X_test.filter(items=columns)
 
-ablations = [
-    [],
-    "avg_num_chars_per_token",
-    "num_chars",
-    "num_sents",
-    "num_alpha",
-    "num_punct",
-    "num_punct2",
-    "num_punct3",
-    "num_contractions",
-    "num_parentheses",
-    # "parentheses_frequency",
-    "num_emoticons",
-    "num_no_vowel_words",
-    "avg_word_length",
-    "num_single_quotes",
-    "num_double_quotes",
-    "num_whitespaces",
-    F.ENGLISH_STOP_WORDS,
-    list(string.ascii_lowercase),
-    F.POS_TAGS,
-]
+    def run_clf(dropped: str | list[str]):
+        X_train_dropped = X_train_filtered.drop(dropped, axis=1)
+        X_test_dropped = X_test_filtered.drop(dropped, axis=1)
+        # clf = RandomForestClassifier(max_depth=30, n_estimators=100, random_state=42)
+        clf = SVC(kernel='linear', C=1, random_state=42)
+        clf.fit(X_train_dropped, y_train)
+        y_pred = clf.predict(X_test_dropped)
 
+        score = f1_score(y_test, y_pred, average="weighted")
+        acc = accuracy_score(y_test, y_pred)
+        return score, acc
 
-groups = [
-    "None",
-    "Average token length",
-    "Total number of characters",
-    "Total number of sentences",
-    "Total number of alphabets",
-    "Total number of punctuation marks",
-    "Total number of double punctuation marks",
-    "Total number of triple punctuation marks",
-    "Total number of contractions",
-    "Total number of parentheses",
-    # "10: Parentheses frequency",
-    "Total number of emoticons",
-    "Total number of no-vowel words",
-    "Average word length",
-    "Total number of single quotes",
-    "Total number of double quotes",
-    "Total number of whitespaces",
-    "Count of each function word",
-    "Count of each letter",
-    "Count of each POS tag",
-]
-
-
-filter = []
-
-
-def perform_ablation(
-    ablation: str | list[str],
-    max_depth=DEFAULT_MAX_DEPTH,
-    n_estimators=DEFAULT_N_ESTIMATORS,
-    random_state=DEFAULT_RANDOM_STATE,
-):
-    X_train_filtered = X_train.drop(ablation, axis=1)
-    X_test_filtered = X_test.drop(ablation, axis=1)
-    # X_train_filtered = X_train_filtered.drop(filter, axis=1)
-    # X_test_filtered = X_test_filtered.drop(filter, axis=1)
-
-    clf = RandomForestClassifier(
-        max_depth=max_depth, n_estimators=n_estimators, random_state=random_state
-    )
-    clf.fit(X_train_filtered, y_train)
-    y_pred = clf.predict(X_test_filtered)
-
-    score = f1_score(y_test, y_pred, average="weighted")
-    acc = accuracy_score(y_test, y_pred)
-    return score, acc
+    return np.array(Parallel(-1)(delayed(run_clf)(x) for x in features)).T
 
 
 def plot_ablation_results(results, groups):
@@ -116,7 +61,7 @@ def plot_ablation_results(results, groups):
     xs = list(string.ascii_uppercase)[: len(groups)]
     labels = [f"{x}: {group}" for x, group in zip(xs, groups)]
     ax.bar(xs, results[0], label=labels)
-    ax.set_ylim(bottom=0.75)
+    ax.set_ylim(bottom=max(np.min(results) - 0.1, 0))
     ax.set_xticks(xs)
     ax.set_xlabel("Remove feature group")
     ax.set_ylabel("F-measure")
@@ -124,34 +69,27 @@ def plot_ablation_results(results, groups):
 
 
 # %%
-results = np.array(Parallel(-1)(delayed(perform_ablation)(x) for x in ablations)).T
-plot_ablation_results(results, groups)
-
-# %%
-fig, ax = plt.subplots(figsize=(10, 10))
-xs = np.arange(len(groups))
-ax.bar(xs, results[0], label=groups)
-
-ax.set_ylim(bottom=0.75)
-ax.set_xticks(xs)
-ax.set_xlabel("Remove feature group")
-ax.set_ylabel("F-measure")
-ax.legend()
-plt.show()
-
-# %%
-plt.figure()
-
-params = np.arange(5, 30, 2)
-results = np.array(Parallel(-1)(delayed(perform_ablation)([], p) for p in params)).T
-
-plt.plot(params, results[0], label="F-measure")
-plt.plot(params, results[1], label="accuracy")
-plt.xlabel("max_depth")
-
-plt.grid()
-plt.legend()
-plt.show()
-
-# print(f"Accuracy: {accuracy * 100:.2f}%")
-# print(f"F-score: {f:.2f}")
+features = {
+    "None":                                     [],
+    "Average token length":                     ["avg_num_chars_per_token"],
+    "Total number of chars":                    ["num_chars"],
+    "Total number of sentences":                ["num_sents"],
+    "Total number of alphabets":                ["num_alpha"],
+    "Total number of punctuation marks":        ["num_punct"],
+    "Total number of double punctuation marks": ["num_punct2"],
+    "Total number of triple punctuation marks": ["num_punct3"],
+    "Total number of contractions":             ["num_contractions"],
+    "Total number of parentheses":              ["num_parentheses"],
+    "Total number of emoticons":                ["num_emoticons"],
+    "Total number of no-vowel words":           ["num_no_vowel_words"],
+    "Average word length":                      ["avg_word_length"],
+    "Total number of single quotes":            ["num_single_quotes"],
+    "Total number of double quotes":            ["num_double_quotes"],
+    "Total number of whitespaces":              ["num_whitespaces"],
+    "Count of each function word":              [f"fn:{word}" for word in F.ENGLISH_STOP_WORDS],
+    "Count of each letter":                     [f"let:{letter}" for letter in string.ascii_letters],
+    "Count of each POS tag":                    [f"pos:{tag}" for tag in F.POS_TAGS],
+    "Count of each phrase":                     [f"{phrase}" for phrase in F.PHRASE.keys()],
+}
+results = perform_ablation(features.values())
+plot_ablation_results(results, features.keys())
